7222771063968e8d8f49c43f7428da66a0357959
Gustavo Paco <gustavopaco@gmail.com>
2021-08-30 20:49:41 -0300

    - *** Importante, aprendendo sobre Data-Binding dos tipos {{ }}, [src], bind-src e [attr.border], [attr.height]

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url : string = "http://loaine.com";
+  cursoAngular : boolean = true;
+  urlImage : string = "https://lorempixel.com/400/200/nature/";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+}
